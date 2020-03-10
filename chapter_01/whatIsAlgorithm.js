544a007dc6288494785292d232d391600c3b3a49
Abdur Rahman Robin <abdurrahmanrobin47@gmail.com>
2020-03-11 01:49:16 +0600

    chapter_01 is added

---
+++ chapter_01/whatIsAlgorithm.js
@@ -0,0 +1,6 @@
+/*
+  Informally, an algorithm is any well-defined computional procedure that takes
+  some value, or set of values, as input and produces some value, or set of values, 
+  as output. An algorithm is thus a sequence of computional steps that transform 
+  the input into the output.
+*/
